9eb81f8fa1dbd38fc6a72fe5de97f04d327c4bc4
Guenther Brunthaler <gb@xquad.emgenxx69lwyn5ctlr4nl64ul.local>
2015-01-03 00:49:15 +0100

    More output for SBOX checking script
    It is now possible to determine which k values are valid vor a given n,
    even if there are some values for k which contradict the assumption.

--- sbox.pl
+++ sbox.pl
@@ -5,30 +5,30 @@
 # Running this script shows that this assumption is not generally true,
 # but it holds for several n including n == 8.
 
-sub bad($) {
-   warn @_;
-   goto back; # What a funny language Perl is! ;-)
-}
-
-$max= $ARGV[0];
-outer: for ($n= 0; $n <= $max; ++$n) {
+$min= $ARGV[0];
+$max= $ARGV[1];
+for ($n= $min; $n <= $max; ++$n) {
    $t= 2 ** $n;
-   for ($k= 2; $k <= $t; ++$k) {
+   k: for ($k= 2; $k <= $t; ++$k) {
       %r= ();
       for ($b= 0; $b < $t; ++$b) {
          $o= ($b + 1) * $k % ($t + 1) - 1;
-         bad "negative result $o for b == $b, k == $k and n == $n"
-            if $o < 0
-         ;
-         bad "duplicate mapped value $o from $b for k == $k and n == $n"
-            if $r{$o}
-         ;
+         if ($o < 0) {
+            warn "negative result $o for b == $b, k == $k and n == $n";
+            next k;
+         }
+         if ($r{$o}) {
+            warn "duplicate mapped value $o from $b for k == $k and n == $n";
+            next k;
+         }
          $r{$o}= 1;
       }
       for ($b= 0; $b < $t; ++$b) {
-         bad "Missing mapped value $b for k == $k and n == $n" unless $r{$b};
+         unless ($r{$b}) {
+            warn "Missing mapped value $b for k == $k and n == $n";
+            next k;
+         }
       }
    }
-   back: next outer;
 }
-print STDERR "All others proved for for n <= $max!\n";
+print STDERR "All other (n, k) proved for for $min <= n <= $max!\n";
